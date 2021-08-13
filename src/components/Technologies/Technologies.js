299a8b5d93d937efb64d8752a930bf6162d41911
Manjot Sidhu <manjot.sidhuu98@gmail.com>
2021-08-13 15:35:42 -0400

    components created

---
+++ src/components/Technologies/Technologies.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Technologies.css';
+
+function Technologies() {
+    return (
+        <div>
+            <h1>Technologies</h1>
+        </div>
+    )
+}
+
+export default Technologies;
